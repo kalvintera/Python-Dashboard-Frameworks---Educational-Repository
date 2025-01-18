a04a63e24039dc0b2540edb7e3a8496c25720020
kalvintera <irem.ebenstein@steincode.com>
2025-01-18 19:52:31 +0100

    update: minor fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,17 +5,23 @@ import plotly.express as px
 # Daten laden
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
 
-# country = st.selectbox('Select a Country', df['country'].unique())
-country = st.sidebar.selectbox('Select a Country', df['country'].unique())
-st.title(f"Streamlit App Demo 	:earth_americas:")
-st.data_editor(df, hide_index=True)
+st.sidebar.subheader("Filter Menu", divider="gray")
 
 # Erstellen eines Dropdown-Menüs für die Auswahl des Landes
+# https://docs.streamlit.io/develop/api-reference/widgets/st.selectbox
+country = st.sidebar.selectbox('Select a Country', df['country'].unique())
 
+
+st.title(f"Streamlit App Demo 	:earth_americas:")
+filtered_df = df[df.country == country]
+st.data_editor(filtered_df, hide_index=True)
 
 # Filtern der Daten basierend auf der Auswahl und Erstellen eines Diagramms
-filtered_df = df[df.country == country]
+
 fig = px.line(filtered_df, x='year', y='gdpPercap', title=f'GDP Per Capita of {country}')
+fig.update_xaxes(type='category')
+fig.update_yaxes(tickprefix="$", tickformat=",.0f")  # GDP mit Währungspräfix, Tausender-Trennzeichen
+# (.0f -> keine Nachkommastellen)
 
 # Anzeigen des Diagramms
 st.plotly_chart(fig)
